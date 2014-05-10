669e454dc1f9ffcba9f484a0e66ae12434b1cd34
Juan Antonio Breña Moral <bren@juanantonio.info>
2014-05-10 21:39:56 +0200

    Adding Scripts to reduce the number of ports opened.

---
+++ chapter15/ev3/ShellScripts/jab-utils.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+#NOTE: Copy this script to /etc/rc0.d and reboot
+
+#Enable syslog
+syslogd -l 8
+
+#Kill telnet service
+start-stop-daemon -K -n telnetd
+
+#Run the http server
+httpd -v -h /home/lejos/www/
+
+exit 1;
